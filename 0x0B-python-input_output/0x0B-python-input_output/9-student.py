1689dca1e0dcce9368ce2ee7a7fa5152b56fdc58
Kenza <kmerouar@gmail.com>
2024-02-20 13:23:14 -0800

    input_output-Python

--- 0x0B-python-input_output/0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/0x0B-python-input_output/9-student.py
@@ -10,8 +10,5 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
-        if attrs is None:
-            return self.__dict__
-        return {key: self.__dict__[key]
-                for key in attrs if key in self.__dict__}
+    def to_json(self):
+        return self.__dict__
