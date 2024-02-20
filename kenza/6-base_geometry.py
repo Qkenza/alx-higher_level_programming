72bed1aa42fb95ff70a271941af1b06618883112
Kenza <kmerouar@gmail.com>
2024-02-20 09:44:47 -0800

    input_output-Python

--- kenza/6-base_geometry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Create a class based 5-base_geometry.py"""
-
-
-class BaseGeometry:
-    """Represent base geometry."""
-
-    def area(self):
-        raise Exception("area() is not implemented")
